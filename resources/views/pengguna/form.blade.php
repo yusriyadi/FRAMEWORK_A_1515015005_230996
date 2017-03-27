e00af3f7f379faf5ab744ed1ee20b5ca4e9d55ee
yusriyadi <yusriyadi23@gmail.com>
2017-03-27 08:39:56 +0800

    posttest41

--- resources/views/pengguna/form.blade.php
+++ resources/views/pengguna/form.blade.php
@@ -1,12 +1,12 @@
 <div class="form-group">
 	<label class="col-sm-2 control-label ">Username</label>
 	<div class="col-sm-10">
-		{!! From::text('username',null,['class'=>'form-control','placeholder'=>"username"]) !!}
+		{!! Form::text('username',null,['class'=>'form-control','placeholder'=>"username"]) !!}
 	</div>
 </div>
 <div>
 	<label class="col-sm-2 control-label">Password</label>
 	<div class="col-sm-10">
-		{!! form::password('password'),['class'=>'form-control','placeholder'=>"Password"]!!}
+		{!! Form::password('password'),['class'=>'form-control','placeholder'=>"Password"]!!}
 	</div>
 </div>
